9fd29f79c704fdf0c0d9eee288149e783e2d4d94
Xinan Shan <>
2021-06-08 03:44:08 +0000

    creating all the class

--- src/CompanySystem/Company.java
+++ src/CompanySystem/Company.java
@@ -1,8 +1,13 @@
 package CompanySystem;
 import java.io.*;
 import CompanySystem.*;
+import java.util.ArrayList;
+
 public class Company {
 
-  public staffName;
-  public staff;
+  private ArrayList<Manager> manager;
+  private ArrayList<Programmers> programmers;
+  private ArrayList<Security> security;
+
+  
 }
