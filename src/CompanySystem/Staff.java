aadb82b67e9a9b8060ff279844f6b6d179868b23
Xinan Shan <>
2021-06-08 03:51:16 +0000

    variables

--- src/CompanySystem/Staff.java
+++ src/CompanySystem/Staff.java
@@ -3,5 +3,10 @@ import CompanySystem.*;
 
 public class Staff{
 
-  private String Name;
+  private String firstName;
+  private String lastName;
+  private int salary;
+  private int workHours; 
+  private String title;
+  
 }
