b260afdeb1fd0478697fc52301220387d71443e9
Xinan Shan <>
2021-06-09 00:59:21 +0000

    staff constuctor

--- src/CompanySystem/Staff.java
+++ src/CompanySystem/Staff.java
@@ -9,4 +9,13 @@ public class Staff{
   private int workHours; 
   private String title;
   
+  //Constructor for staff
+  public Staff(String fName, String lName, int sala, int Whour, String titl){
+    firstName = fName;
+    lastName = lName;
+    salary = sala;
+    workHours = Whour;
+    title = titl;
+  }
+  
 }
