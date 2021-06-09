2242f6ff4e70cbea36ff3e0462256ec94b855eb1
Xinan Shan <82896753+XinanShan@users.noreply.github.com>
2021-06-09 20:47:06 +0000

    block text

--- src/CompanySystem/Main.java
+++ src/CompanySystem/Main.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 
 /**
 * A program that allows the user to view the information of the staff for different position
-* The program displays a list of information that changes as 
-* @author: N. Lee 
+* The program displays a list of information that changes as the user enter different positions
+* @author: Andy Shan
 * 
 */
 public class Main {
