2f91978567d1f53214763c6a25fff990fd055831
Xinan Shan <>
2021-06-09 04:08:13 +0000

    Constructor of security

--- src/CompanySystem/Security.java
+++ src/CompanySystem/Security.java
@@ -1,3 +1,16 @@
 package CompanySystem;
 import CompanySystem.*;
 
+public class Security extends Staff {
+  private String uniformColour;
+  private int workoutDaysPerWeek;
+  private Boolean carryGun;
+
+  public Security(String fName, String lName, int sala, int Whour, String titl, String UniCol, int WorOutDay, boolean CarGun){
+    super(fName, lName, sala, Whour, titl);
+    uniformColour = UniCol;
+    workoutDaysPerWeek = WorOutDay;
+    carryGun = CarGun;
+  }
+  
+}
