15b1515dc925633b61c0d78162ed0917d86488b8
Xinan Shan <82896753+XinanShan@users.noreply.github.com>
2021-06-10 01:51:09 +0000

    programmer objects

--- src/CompanySystem/Main.java
+++ src/CompanySystem/Main.java
@@ -12,14 +12,14 @@ public class Main {
   public static void main(String[] args) throws IOException{
     Manager wendy = new Manager("Wendy", "Smith", 14000, 7, "Manager", 2, "Coldheart");
     Manager rick = new Manager("Rick", "Sanchez", 13000, 6, "Manager", 3, "Sweet Business")
-    Programmer tammmy = new Programmer("Tammy", "Guetermann");
-    Programmer toby = new Programmer("Toby", "Matthews");
-    Programmer tammmy = new Programmer("Alex", "Hirsch");
-    Programmer sarah = new Programmer("Sarah", "Chalke");
-    Programmer zeep = new Programmer("Zeep", "Xanflorp");
-    Security chris = new Security("Chris", "Parnell", );
-    Security dan = new Security("Dan", "Harmon");
-    Security cassie = new Security("Cassie", "Steele", );
+    Programmer tammmy = new Programmer("Tammy", "Guetermann", 9000, 7, "Programmer", "C++", 518761757, 7);
+    Programmer toby = new Programmer("Toby", "Matthews", 10000, 8, "Programmer", "Python", 518547123, 10);
+    Programmer alex = new Programmer("Alex", "Hirsch", 11000, 8, "Programmer", "Python", 518638422, 12);
+    Programmer sarah = new Programmer("Sarah", "Chalke", 8000, 7, "Programmer", "Java", 518622487, 6);
+    Programmer zeep = new Programmer("Zeep", "Xanflorp", 7600, 6, "Programmer", "Python", 518147963, 5);
+    Security chris = new Security("Chris", "Parnell", 7600, 6, "Security Guard", ");
+    Security dan = new Security("Dan", "Harmon", 7600, 6, "Security Guard", ");
+    Security cassie = new Security("Cassie", "Steele", 7600, 6, "Security Guard", "");
     
   }
 }
