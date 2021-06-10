d9e0b2df98877e483500cf005c5f18ea56cf0b14
Xinan Shan <82896753+XinanShan@users.noreply.github.com>
2021-06-10 00:30:05 +0000

    object

--- src/CompanySystem/Main.java
+++ src/CompanySystem/Main.java
@@ -7,10 +7,19 @@ import java.util.ArrayList;
 * A program that allows the user to view the information of the staff for different position
 * The program displays a list of information that changes as the user enter different positions
 * @author: Andy Shan
-* 
 */
 public class Main {
   public static void main(String[] args) throws IOException{
+    Manager wendy = new Manager("Wendy", "Smith", );
+    Manager rick = new Manager("Rick", "Sanchez", )
+    Programmer tammmy = new Programmer("Tammy", "Guetermann");
+    Programmer toby = new Programmer("Toby", "Matthews");
+    Programmer tammmy = new Programmer("Alex", "Hirsch");
+    Programmer sarah = new Programmer("Sarah", "Chalke");
+    Programmer sarah = new Programmer("Zeep", "Xanflorp");
+    Security chris = new Security("Chris", "Parnell", );
+    Security dan = new Security("Dan", "Harmon");
+    Security cassie = new Security("Cassie", "Steele", );
     
   }
 }
